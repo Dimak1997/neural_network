15aa09d83cc31d52778343526df83fd33185dc1a
Dimak1997 <45372428+Dimak1997@users.noreply.github.com>
2018-11-27 04:20:33 +0700

    Update test.py

--- CNN/scripts_to_run/test.py
+++ CNN/scripts_to_run/test.py
@@ -1,1 +1,19 @@
+from keras.models import model_from_json
+from keras.models import load_model
+import cv2
+import numpy as np
+import pickle
 
+#Загрузка тестовой выборки из файла
+f = open(r'file.txt', 'rb')
+X_test = pickle.load(f)
+y_test = pickle.load(f)
+f.close()
+
+#Загрузка модели нейронной сети из файла
+filepath = '/home/andrey/PycharmProjects/flowers/venv/model.hf5'
+loaded_model = load_model(filepath,custom_objects=None,compile=True)
+
+#Результат проверки на тестовой выборке
+score = loaded_model.evaluate(X_test, y_test)
+print(score)
